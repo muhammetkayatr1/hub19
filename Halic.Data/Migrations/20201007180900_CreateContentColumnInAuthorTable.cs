cadc69a3f5cd5fd197eb229208ba01022a5a9c8e
muhammetkayatr1 <69511438+muhammetkayatr1@users.noreply.github.com>
2020-10-19 10:51:16 +0300

    first commit

---
+++ Halic.Data/Migrations/20201007180900_CreateContentColumnInAuthorTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Halic.Data.Migrations
+{
+    public partial class CreateContentColumnInAuthorTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Authors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Authors");
+        }
+    }
+}
